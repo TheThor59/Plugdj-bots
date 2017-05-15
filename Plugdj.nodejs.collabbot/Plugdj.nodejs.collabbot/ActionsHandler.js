c69e67f7babb3a521b93d5eaa1d70e3b726b97d0
TheThor59 <ethanhol@yahoo.fr>
2017-05-16 00:11:37 +0200

    Updated the code to change project style and code process

---
+++ Plugdj.nodejs.collabbot/Plugdj.nodejs.collabbot/ActionsHandler.js
@@ -0,0 +1,43 @@
+﻿(function () {
+    'use strict';
+    var PlugAPI = require('plugapi');
+
+    function ActionHandler() {
+        this.params = null;
+        this.connection = null;
+    }
+
+    ActionHandler.prototype.connect = function (params, onSuccess, onFailure) {
+        this.params = params;
+        if (!params.user || !params.password || !params.roomslug) {
+            throw new Error("Connect - Parameters not correctly provided");
+        }
+        let self = this;
+        console.log("Connect - Starting connection process");
+        new PlugAPI({
+            email: this.params.user,
+            password: this.params.password,
+        }, function (err, connection) {
+            if (err) {
+                console.log("Connect - Error during connection process");
+                onFailure(err);
+            } else {
+                console.log("Connect - Connection success, opening the room " + self.params.roomslug);
+                self.connection = connection;
+                self.connection.connect(self.params.roomslug);
+                console.log("Connect - End of connection process");
+                onSuccess();
+            }
+        });
+    }
+
+    ActionHandler.prototype.registerHandlers = function () {
+        console.log("Register - Registring in " + this.params.roomslug);
+        // Chat registration
+        var self = this;
+        this.connection.on('chat', function (data) {
+            console.log(self.params.roomslug + ": " + data.from + "> " + data.message);
+        });
+    }
+    module.exports = new ActionHandler();
+})();
---
+++ Plugdj.nodejs.collabbot/Plugdj.nodejs.collabbot/server.js
@@ -0,0 +1,30 @@
+﻿(function () {
+    'use strict';
+    var ActionsHandler = require('./ActionsHandler.js');
+
+
+    var configuration = {
+        user: 'plugbot',
+        password: '',
+        roomslug: ''
+    }
+
+    ActionsHandler.connect(configuration, function () {
+        //Success
+        ActionsHandler.registerHandlers();
+    },
+    function() {
+    });
+
+})();
+
+//            bot.sendChat("Collab Bot is started");
+
+
+
+
+//            //Join the booth
+//            //console.log(globalStore.room + ": Joining booth.");
+//            //bot.joinBooth(function () {
+//            //    console.log(globalStore.room + ": Booth joined.");
+//            //});
